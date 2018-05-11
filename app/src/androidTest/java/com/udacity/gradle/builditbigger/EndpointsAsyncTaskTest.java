3dfb20ec617cca211d8bcf9c231209a6a50fb4dc
Teresa Ng <teresa.ng@starlingbank.com>
2018-05-11 21:03:53 +0100

    Add countdown latch to test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -5,6 +5,9 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -12,14 +15,16 @@ import static org.junit.Assert.assertNotNull;
 public class EndpointsAsyncTaskTest {
 
   @Test
-  public void asyncTaskAlwaysReturnsExpectedJoke() {
+  public void asyncTaskAlwaysReturnsExpectedJokeWhenBackEndIsConnected() throws InterruptedException {
+    final CountDownLatch signal = new CountDownLatch(1);
     EndpointsAsyncTask.OnTaskCompleted taskCompleted = joke -> {
       assertNotNull(joke);
       assertEquals("This is totally a funny joke", joke);
+      signal.countDown();// notify the count down latch
     };
-    // Execute task
-    EndpointsAsyncTask movieTask = new EndpointsAsyncTask(taskCompleted);
-    movieTask.execute();
-  }
 
+    EndpointsAsyncTask movieTask = new EndpointsAsyncTask(taskCompleted);
+    getInstrumentation().runOnMainSync(movieTask::execute);
+    signal.await();// wait for callback
+  }
 }
